918102d2e3b2764a96fa1c25f35d5166234fcdeb
iryna vasylenko <irina.v@twsgo.com>
2022-07-20 18:07:23 +0100

    initial commit

--- App.js
+++ App.js
@@ -4,8 +4,6 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,8 +11,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
